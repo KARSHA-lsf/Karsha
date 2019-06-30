4f67f94bc15b87db42cbc018fc459271ff796cd2
tharindu99 <madushanka.best@gmail.com>
2019-06-30 23:30:24 +0530

    project initialized

---
+++ service-resmbs/src/main/java/com/karsha/resmbs/ServiceResMbsApplication.java
@@ -0,0 +1,13 @@
+package com.karsha.resmbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceResMbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceResMbsApplication.class, args);
+    }
+
+}
